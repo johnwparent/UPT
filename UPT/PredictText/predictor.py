f19608f521c7317ae58404bac5bdba9b4167f851
OwenTalmo <oetalmage16@gmail.com>
2021-05-12 14:40:18 -0400

    pseudocode added

--- UPT/PredictText/predictor.py
+++ UPT/PredictText/predictor.py
@@ -26,6 +26,27 @@ def remove_words(list, bg, tg):
             if badword in b_entry:
                 b_entry.delete()
 
+def get_restarters(data):
+    # pick 10 random words from bag of words and stick them in an array
+    restarters = {}
+    for i in range(0,10,1):
+        restarters.append() #pick a random number and add bag of words entry to restarters
+    return restarters
+
+
+
+def generate_suggestions(word, prev_words):
+    # calls other functions to return 3 selected words
+    # 1. pull top 30 trigrams from data
+    tgs = collect_trigrams(data)
+    # 2. get top bigrams using word
+    bgs = collect_bigrams(word)
+    # 3. remove words in previous words list from lists
+    remove_words(prev_words,bgs,tgs)
+    # 4. create a list to populate with top 3 suggested words
+        # if word and word-1 are the first 2 entries in a trigram, that should be #1
+        # fill remaining entries with 2 most common bigrams
+        # fill remaining spots with random words
 
 
 
