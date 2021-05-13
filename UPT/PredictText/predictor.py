49089a7ea84df2a2afb9f3ad94d6399c26ea6e38
OwenTalmo <oetalmage16@gmail.com>
2021-05-12 21:52:00 -0400

    Updated generate_suggestions

--- UPT/PredictText/predictor.py
+++ UPT/PredictText/predictor.py
@@ -26,19 +26,16 @@ def remove_words(list, bg, tg):
             if badword in b_entry:
                 b_entry.delete()
 
-def get_restarters(data):
-    # pick 10 random words from bag of words and stick them in an array
-    restarters = {}
-    for i in range(0,10,1):
-        restarters.append() #pick a random number and add bag of words entry to restarters
-    return restarters
+def get_restarter(data):
+    # pick a random word from bag of words and return it
+    restarter = "owen"
+    return restarter
 
 
-
-def generate_suggestions(word, prev_words):
-    # calls other functions to return 3 selected words
-    # 0. read in data
-
+def generate_suggestions(words, context):
+    # set word to last element in words
+    word = words[-1]
+    prev_words = words[len(words)-5:len(words)-1]
     # 1. pull top 30 trigrams from data
     tgs = collect_trigrams(data)
     # 2. get top bigrams using word
@@ -46,9 +43,22 @@ def generate_suggestions(word, prev_words):
     # 3. remove words in previous words list from lists
     remove_words(prev_words,bgs,tgs)
     # 4. create a list to populate with top 3 suggested words
-        # if word and word-1 are the first 2 entries in a trigram, that should be #1
-        # fill remaining entries with 2 most common bigrams
-        # fill remaining spots with random words
+    suggestion_list = {}
+    # if word and word-1 are the first 2 entries in a trigram, that should be #1
+    for trigram in tgs:
+        if str(prev_words[-1] + " " + word).lower() == str(trigram[0][0] + " " + trigram[0][0]).lower():
+            suggestion_list.append(trigram[0][2])
+    # fill remaining entries with 2 most common bigrams
+    n = len(suggestion_list)
+    for i in range(0, 3-n,1):
+        # if bgs[i] is of type [[word1,word2],freq], this gets word2
+        suggestion_list.append(bgs[i][0][1])        
+    # if there are still less than 3 elements in suggestion_list, fill it with random words
+    while len(suggestion_list) < 3:
+        suggestion_list.append(context)
+
+
+
 
 
 
